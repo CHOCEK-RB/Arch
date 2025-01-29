1480d6df18f8210e55bea9b5a53ba8891ed98ed3
CHOCEK <choceksalas@gmail.com>
2025-01-28 21:09:27 -0500

    inicio

---
+++ install.bash
@@ -0,0 +1,8 @@
+#instalar dependencias
+yay -Syu bspwm sxhkd feh lightdm lightdm-gtk-greetera picom kitty spotify firefox brave youtube-music-bin vscode dolphin rofi polybar
+
+
+mv ./.xprofile ~/
+
+systemctl enable lightdm.service
+
