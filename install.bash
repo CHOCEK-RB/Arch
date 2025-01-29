c70c46ab1d36679e8318f5adc337e3bb6a7e5ab2
CHOCEK <choceksalas@gmail.com>
2025-01-28 21:26:42 -0500

    inicio

--- install.bash
+++ install.bash
@@ -2,3 +2,5 @@
 yay -Syu bspwm sxhkd feh lightdm lightdm-gtk-greetera picom kitty spotify firefox brave youtube-music-bin vscode dolphin rofi polybar
 
 systemctl enable lightdm.service
+
+mv ./Templates/* ~/Templates/
