79476aac111dbfb67d86bc37d21e12fa48028a85
CHOCEK <choceksalas@gmail.com>
2025-01-28 21:34:17 -0500

    inicio

--- install.bash
+++ install.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #instalar dependencias
-yay -Syu bspwm sxhkd feh lightdm lightdm-gtk-greeter picom kitty spotify firefox brave youtube-music-bin vscode dolphin rofi polybar nvim
+yay -Syu bspwm sxhkd feh lightdm lightdm-gtk-greeter picom kitty spotify firefox brave youtube-music-bin vscode dolphin rofi polybar neovim
 systemctl enable lightdm.service
 
 mv ./Templates/* ~/Templates/
